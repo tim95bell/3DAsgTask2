c77e514b39848e58c6b7fbed3eb267304774eb95
Tim Bell <Tim95Bell@gmail.com>
2016-09-26 00:59:36 +1000

    refactor

--- Objects.js
+++ Objects.js
@@ -1,32 +1,38 @@
-
-
 var vertices = [];
-
+var normals = [];
 
 /* ----------------  RECT ---------------- */
+// defined from center
 function Rect(loc, width, height, color){
   this.color = color;
-  // this.trs = mat4();
+  this.loc = loc;
   this.width = width;
   this.height = height;
-  this.loc = loc;
   // this.trs = mult( scalem(width, height, 1), translate(loc[0], loc[1], loc[2]) );
+  // this.trs = setTrs(loc, size, rot);
   this.trs = mult( translate(loc[0], loc[1], loc[2]), scalem(width, height, 1) );
-  // this.setTrs();
 }
 Rect.NV = 4;
 Rect.startIndex = vertices.length;
-vertices.push( vec3(0, 0, 0) );
-vertices.push( vec3(1, 0, 0) );
-vertices.push( vec3(0, 1, 0) );
-vertices.push( vec3(1, 1, 0) );
+vertices.push( vec3(-0.5, -0.5, 0) );
+vertices.push( vec3(0.5, -0.5, 0) );
+vertices.push( vec3(-0.5, 0.5, 0) );
+vertices.push( vec3(0.5, 0.5, 0) );
+normals.push( normalize(vec3(0, 0, 1)) );
+normals.push( normalize(vec3(0, 0, 1)) );
+normals.push( normalize(vec3(0, 0, 1)) );
+normals.push( normalize(vec3(0, 0, 1)) );
 
+Rect.prototype.setTrs = function(trs){
+  this.trs = trs;
+}
 Rect.prototype.draw = function(){
-  // var m = mat4(2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
-  var m = mat4();
     gl.uniformMatrix4fv(modelViewLoc, false, flatten(this.trs) );
     gl.uniform4fv(colLoc, flatten(this.color));
     gl.drawArrays(gl.TRIANGLE_STRIP, Rect.startIndex, Rect.NV);
+}
+Rect.prototype.setTrs = function(trs){
+  this.trs = trs;
 }
 // Rect.prototype.setTrs = function(trs){
 //   this.trs = mult( translate(this.loc[0], this.loc[1], this.loc[2]), scalem(this.width, this.height, 1) );
@@ -37,25 +43,25 @@ Rect.prototype.draw = function(){
 
 /* ----------------  CUBE ---------------- */
 // loc, widht, length, height, color
+//defined from center
 function Cube(loc, width, length, height, color){
   this.color = color;
   var s = scalem(width, length, height);
   var t = translate(loc[0], loc[1], loc[2]);
   var ts = mult( t, s );
-  console.log(loc);
   this.trs = ts;
 }
 Cube.startIndex = vertices.length;
 var i = [
-  vec3(0, 1, 0),
-  vec3(0, 0, 0),
-  vec3(1, 0, 0),
-  vec3(1, 1, 0),
+  vec3(-0.5, 0.5, -0.5),
+  vec3(-0.5, -0.5, -0.5),
+  vec3(0.5, -0.5, -0.5),
+  vec3(0.5, 0.5, -0.5),
 
-  vec3(0, 1, 1),
-  vec3(0, 0, 1),
-  vec3(1, 0, 1),
-  vec3(1, 1, 1)
+  vec3(-0.5, 0.5, 0.5),
+  vec3(-0.5, -0.5, 0.5),
+  vec3(0.5, -0.5, 0.5),
+  vec3(0.5, 0.5, 0.5)
 ];
 // face takes a, b, c, d in anti clockwise order
 function face(a, b, c, d){
@@ -68,20 +74,75 @@ function face(a, b, c, d){
   vertices.push(i[d]);
 }
 Cube.NV = 6*6;
+//front
 face(0, 1, 2, 3);
+// right
 face(3, 2, 6, 7);
+//back
 face(7, 6, 5, 4);
+//left
 face(4, 5, 1, 0);
+//top
 face(4, 0, 3, 7);
+//bottom
 face(1, 5, 6, 2);
+
+// front, right, back, left, top, bottom
+//front
+normals.push( normalize(vec3(0, -1, 0)) );
+normals.push( normalize(vec3(0, -1, 0)) );
+normals.push( normalize(vec3(0, -1, 0)) );
+normals.push( normalize(vec3(0, -1, 0)) );
+normals.push( normalize(vec3(0, -1, 0)) );
+normals.push( normalize(vec3(0, -1, 0)) );
+//right
+normals.push( normalize(vec3(1, 0, 0)) );
+normals.push( normalize(vec3(1, 0, 0)) );
+normals.push( normalize(vec3(1, 0, 0)) );
+normals.push( normalize(vec3(1, 0, 0)) );
+normals.push( normalize(vec3(1, 0, 0)) );
+normals.push( normalize(vec3(1, 0, 0)) );
+//back
+normals.push( normalize(vec3(0, 1, 0)) );
+normals.push( normalize(vec3(0, 1, 0)) );
+normals.push( normalize(vec3(0, 1, 0)) );
+normals.push( normalize(vec3(0, 1, 0)) );
+normals.push( normalize(vec3(0, 1, 0)) );
+normals.push( normalize(vec3(0, 1, 0)) );
+//left
+normals.push( normalize(vec3(-1, 0, 0)) );
+normals.push( normalize(vec3(-1, 0, 0)) );
+normals.push( normalize(vec3(-1, 0, 0)) );
+normals.push( normalize(vec3(-1, 0, 0)) );
+normals.push( normalize(vec3(-1, 0, 0)) );
+normals.push( normalize(vec3(-1, 0, 0)) );
+//top
+normals.push( normalize(vec3(0, 0, 1)) );
+normals.push( normalize(vec3(0, 0, 1)) );
+normals.push( normalize(vec3(0, 0, 1)) );
+normals.push( normalize(vec3(0, 0, 1)) );
+normals.push( normalize(vec3(0, 0, 1)) );
+normals.push( normalize(vec3(0, 0, 1)) );
+//bottom
+normals.push( normalize(vec3(0, 0, -1)) );
+normals.push( normalize(vec3(0, 0, -1)) );
+normals.push( normalize(vec3(0, 0, -1)) );
+normals.push( normalize(vec3(0, 0, -1)) );
+normals.push( normalize(vec3(0, 0, -1)) );
+normals.push( normalize(vec3(0, 0, -1)) );
+
+
 Cube.prototype.draw = function(){
     gl.uniformMatrix4fv(modelViewLoc, false, flatten(this.trs) );
     gl.uniform4fv(colLoc, flatten(this.color));
     gl.drawArrays(gl.TRIANGLES, Cube.startIndex, Cube.NV);
 }
+Cube.prototype.setTrs = function(trs){
+  this.trs = trs;
+}
 
 
-/* ----------------  CYLINDER ---------------- */
+/* ----------------  CIRCLE ---------------- */
 function Circle(color){
   this.color = color;
   this.trs = mat4();//translate(0, 0.1, 0);
@@ -102,9 +163,13 @@ vertices.push(vertices[Circle.startIndex+1])
 
 Circle.prototype.draw = function(){
   gl.uniformMatrix4fv(modelViewLoc, false, flatten(this.trs) );
-  gl.uniform4fv(colLoc, flatten(this.color))
+  gl.uniform4fv(colLoc, flatten(this.color));
   gl.drawArrays(gl.TRIANGLE_FAN, Circle.startIndex, Circle.NV);
 }
+
+for(var i = 0; i < Circle.NV; ++i)
+  normals.push( normalize(vec3(0, 0, 1)) );
+
 
 /* ----------------  CYLINDER ---------------- */
 
@@ -122,9 +187,14 @@ angle = Math.PI*2/((Cylinder.NV-2)/2);
 for(var i = 0; i < (Cylinder.NV-2)/2; ++i){
   vertices.push( vec3(Math.cos(i*angle)*0.5, Math.sin(i*angle)*0.5, -0.5) );
   vertices.push( vec3(Math.cos(i*angle)*0.5, Math.sin(i*angle)*0.5, 0.5) );
+
+  normals.push( normalize(vec3(Math.cos(i*angle), Math.sin(i*angle), 0)) );
+  normals.push( normalize(vec3(Math.cos(i*angle), Math.sin(i*angle), 0)) );
 }
 vertices.push( vec3(Math.cos(0*angle)*0.5, Math.sin(0*angle)*0.5, -0.5) );
 vertices.push( vec3(Math.cos(0*angle)*0.5, Math.sin(0*angle)*0.5, 0.5) );
+  normals.push( normalize(vec3(Math.cos(0*angle), Math.sin(0*angle), 0)) );
+  normals.push( normalize(vec3(Math.cos(0*angle), Math.sin(0*angle), 0)) );
 
 Cylinder.prototype.draw = function(){
   gl.uniformMatrix4fv(modelViewLoc, false, flatten(this.trs) );
@@ -150,11 +220,16 @@ function Cone(color){
 Cone.startIndex = vertices.length;
 Cone.NV = 50;
 var angle = Math.PI*2/(Cone.NV-1-1);
-vertices.push( vec3(0, 0, 1) );
+vertices.push( vec3(0, 0, 0.5) );
+  normals.push( normalize(vec3(0, 0, 1)) );
 for(var i = 0; i < Cone.NV-1-1; ++i){
-  vertices.push( vec3( Math.cos(i*angle)*0.5, Math.sin(i*angle)*0.5, 0) );
+  vertices.push( vec3( Math.cos(i*angle)*0.5, Math.sin(i*angle)*0.5, -0.5) );
+
+  // TODO: these normals need to be fixed
+  normals.push( normalize(vec3(Math.cos(i*angle), Math.sin(i*angle), 0)) );
 }
-vertices.push( vec3( Math.cos(0*angle)*0.5, Math.sin(0*angle)*0.5, 0) );
+vertices.push( vec3( Math.cos(0*angle)*0.5, Math.sin(0*angle)*0.5, -0.5) );
+normals.push( normalize(vec3(Math.cos(0*angle), Math.sin(0*angle), 0)) );
 
 Cone.prototype.draw = function(){
   gl.uniformMatrix4fv(modelViewLoc, false, flatten(this.trs) );
@@ -164,7 +239,7 @@ Cone.prototype.draw = function(){
 }
 Cone.prototype.setTrs = function(trs){
   this.trs = trs;
-  this.circle.trs = trs;
+  this.circle.trs = mult( trs, translate( 0, 0, -0.5) );
 }
 
 /* ----------------  TREE ---------------- */
@@ -174,7 +249,7 @@ function Tree(loc, diam, height){
   this.height = height;
   this.trs = mat4();
   this.cylinder = new Cylinder( vec4(0.3, 0.2, 0.04, 1.0) );
-  this.cone = new Cone( vec4(0.3, 0.9, 0.2, 1.0) );
+  this.cone = new Cone( vec4(0.1, 0.6, 0.1, 1.0) );
   var s = scalem(diam, diam, height);
   var t = translate(loc[0], loc[1], 0);
   this.setTrs( mult(t, s) );
@@ -195,72 +270,74 @@ Tree.prototype.setTrs = function(trs){
 
   //cone
   var coneS = scalem(4, 4, 0.6);
-  var coneT = translate(0, 0, 1);
+  var coneT = translate(0, 0, 1 + (0.6*0.5)); // 0.6*0.3 is accounting for the scaling
+
   this.cone.setTrs( mult(this.trs, mult(coneT, coneS)) );
   // console.log(coneT);
 }
 
 /* ----------------  ROOF ---------------- */
-function Roof(loc, width, length, height, color){
-  this.color = color;
-
-}
-Roof.NV = ;
-Roof.startIndex = vertices.length;
+// function Roof(loc, width, length, height, color){
+//   this.color = color;
+//
+// }
+// Roof.NV = ;
+// Roof.startIndex = vertices.length;
 
 
 
 /* ----------------  PARTHENON ---------------- */
 function Parthenon(vLoc, fScale){
   this.loc = vLoc;
-  this.width = 8*fScale;
-  this.length = 13*fScale;
-  this.height = 8*fScale;
+  this.width = Parthenon.WIDTH*fScale;
+  this.length = Parthenon.LENGTH*fScale;
+  this.height = Parthenon.HEIGHT*fScale;
   this.color = vec4(0.5, 0.5, 0.5, 1);
   this.base = new Rect( vec3(0, 0, 0.01), 8, 13, this.color );
   this.pillars = [];
   this.lintels = [];
   this.trs = null;
   // along width
-  var diam = this.width/4/2;
+  var diam = this.length/4/2;
   for(var i = 0; i < 4; ++i){
-    var x = (diam/2) + (i*(diam+this.width/3/2));
-    var topY = this.length-(diam/2);
-    var bottomY = diam/2;
+    var y = this.loc[1]-this.length/2 + (diam/2) + (i*(diam+this.length/3/2));
+    var topX = this.loc[0]+this.width/2-(diam/2);
+    var bottomX = this.loc[0]-this.width/2 + diam/2;
     // var top = new Cylinder(this.color);
     // var bottom = new Cylinder(this.color);
     var top = new Cylinder(vec4(0,0,0,1));
     var bottom = new Cylinder(vec4(0,0,0,1));
     var s = scalem(diam, diam, this.height); // maybe this shouldnt be height
-    top.trs = mult( translate(x, topY, this.height/2), s );
-    bottom.trs = mult( translate(x, bottomY, this.height/2), s );
+    top.trs = mult( translate(topX, y, this.height/2), s );
+    bottom.trs = mult( translate(bottomX, y, this.height/2), s );
     this.pillars.push(top);
     this.pillars.push(bottom);
   }
   // along length
+  var gap = (this.width-diam*6)/5;
   for(var i = 1; i < 5; ++i){
-    var y = (diam/2) + (i*(diam+this.length/5/2));
-    var topX = this.width-(diam/2);
-    var bottomX = diam/2;
+    var x = this.loc[0]-this.width/2 + (diam/2) + (i*(diam+gap));//diam+this.width/5/2));
+    var topY = this.loc[1]+this.length/2-(diam/2);
+    var bottomY = this.loc[1]-this.length/2+diam/2;
     // var top = new Cylinder(this.color);
     // var bottom = new Cylinder(this.color);
     var top = new Cylinder(vec4(0,0,0,1));
     var bottom = new Cylinder(vec4(0,0,0,1));
     var s = scalem(diam, diam, this.height); // maybe this shouldnt be height
-    top.setTrs( mult( translate(topX, y, this.height/2), s ) );
-    bottom.setTrs( mult( translate(bottomX, y, this.height/2), s ) );
+    top.setTrs( mult( translate(x, topY, this.height/2), s ) );
+    bottom.setTrs( mult( translate(x, bottomY, this.height/2), s ) );
     this.pillars.push(top);
     this.pillars.push(bottom);
   }
   // lintels
   // loc, widht, length, height, color
-  this.lintels.push( new Cube( vec3(this.loc[0], this.loc[1], this.height),
+  this.lintels.push( new Cube( vec3(this.loc[0], this.loc[1]-this.length/2+diam/2, this.height+0.25*fScale),
                     this.width, diam, 0.5*fScale, this.color) );
-  this.lintels.push( new Cube( vec3(this.loc[0], this.loc[1]+this.length-diam, this.height),
+  this.lintels.push( new Cube( vec3(this.loc[0], this.loc[1]+this.length/2-diam/2, this.height+0.25*fScale),
                     this.width, diam, 0.5*fScale, this.color) );
-  this.lintels.push( new Cube( vec3(this.loc[0], this.loc[1], this.height),
+  this.lintels.push( new Cube( vec3(this.loc[0]-this.width/2+diam/2, this.loc[1], this.height+0.25*fScale),
                     diam, this.length, 0.5*fScale, this.color) );
-  this.lintels.push( new Cube( vec3(this.loc[0]+this.width-diam, this.loc[1], this.height),
+  this.lintels.push( new Cube( vec3(this.loc[0]+this.width/2-diam/2, this.loc[1], this.height+0.25*fScale),
                     diam, this.length, 0.5*fScale, this.color) );
 
 
@@ -282,6 +359,13 @@ Parthenon.prototype.draw = function(){
 Parthenon.prototype.setTrs = function(trs){
   this.trs = trs;
 
-  var baseT = translate(0, 0, 0.01);
+  var baseT = translate(0, 0, -this.height/2+0.01);
   this.base.trs = mult(baseT, this.trs);
 }
+// Parthenon.WIDTH = 8;
+// Parthenon.HEIGHT = 8;
+// Parthenon.LENGTH = 13;
+
+Parthenon.WIDTH = 13;
+Parthenon.HEIGHT = 8;
+Parthenon.LENGTH = 8;
--- main.js
+++ main.js
@@ -3,115 +3,46 @@
 var canvas;
 var gl;
 
+//perspective
 var near = 1.0;
 var far = 300;
 
 var fovy = 27.0;
 var aspectRatio;
 
-var worldView, projection, modelView;
-var worldViewLoc, projLoc, colLoc, modelViewLoc;
-
 var eye = vec3(0.0, -48.0, 2.0);
 var at = vec3(0.0, 0.0, 2.0);
 var up = vec3(0.0, 0.0, 1.0);
 
-var paused = true;
+// uniforms
+var worldView, projection;
+var worldViewLoc, projLoc, colLoc, modelViewLoc;
 
+// movement
 var left, right, foward, backward;
 left = right = foward = backward = false;
-
-// TODO: fix so i can look up and down, without effecting the vertical direction of the movement.
+// foward move is the equivilant of at, except does not get rotated up and down
 var fowardMove;
 
+// pause and mouse loc system
+var paused = true;
 var clickToPlay;
 
+// objects
+//trees
 var trees = [];
-
-// loc, width, height, color
+//grass
 var grassSize = 100;
-// var grassVertices = [
-//   vec3(grassSize/2.0, -grassSize/2, 0.0),
-//   vec3(grassSize/2.0, grassSize/2.0, 0.0),
-//   vec3(-grassSize/2.0, grassSize/2, 0.0),
-//   vec3(-grassSize/2.0, -grassSize/2, 0.0)
-// ];
-// var grass = new Rect( grassVertices, vec4(0.4, 0.8, 0.2, 1.0) );
-var grass = new Rect( vec3(-grassSize/2, -grassSize/2, 0), grassSize, grassSize, vec4(0.4, 0.8, 0.2, 1.0) );
-
-// var path1Vertices = [
-//   vec3(-3.0, -50.0, 0.001),
-// 	vec3(-3.0, 50.0, 0.001),
-// 	vec3(-5.0, 50.0, 0.001),
-// 	vec3(-5.0, -50.0, 0.001)
-// ];
-// var path1 = new Rect( path1Vertices, vec4(0.0, 0.0, 0.0, 1.0) );
-var path1 = new Rect( vec3(-5, -grassSize/2, 0.001), 2, grassSize, vec4(0.0, 0.0, 0.0, 1.0) );
-
-// var path2Vertices = [
-//   vec3(3.0, -50.0, 0.001),
-// 	vec3(3.0, 50.0, 0.001),
-// 	vec3(5.0, 50.0, 0.001),
-// 	vec3(5.0, -50.0, 0.001)
-// ];
-//var path2 = new Rect( path2Vertices, vec4(0.0, 0.0, 0.0, 1.0) );
-var path2 = new Rect( vec3(3, -grassSize/2, 0.001), 2, grassSize, vec4(0.0, 0.0, 0.0, 1.0) );
-
-var parthenon = new Parthenon(vec3(0, 0, 0), 0.3);
-
-// var grass = {
-//   vertices: [
-//   vec3(50.0, -50.0, 0.0),
-// 	vec3(50.0, 50.0, 0.0),
-// 	vec3(-50.0, 50.0, 0.0),
-// 	vec3(-50.0, -50.0, 0.0)
-//   ],
-//   NV: 4,
-//   color: vec4(0.4, 0.8, 0.2, 1.0),
-//   startIndex: 0,
-//   draw: function(){
-//     gl.uniform4fv(colLoc, flatten(this.color));
-//     gl.drawArrays(gl.TRIANGLE_FAN, this.startIndex, this.NV);
-//   }
-// };
-// vertices = vertices.concat(grass.vertices);
-//
-// var path1 = {
-//   vertices: [
-//   vec3(-3.0, -50.0, 0.001),
-// 	vec3(-3.0, 50.0, 0.001),
-// 	vec3(-5.0, 50.0, 0.001),
-// 	vec3(-5.0, -50.0, 0.001)
-//   ],
-//   NV: 4,
-//   color: vec4(0.0, 0.0, 0.0, 1.0),
-//   startIndex: 4,
-//   draw: function(){
-//     gl.uniform4fv(colLoc, flatten(this.color));
-//     gl.drawArrays(gl.TRIANGLE_FAN, this.startIndex, this.NV);
-//   }
-// };
-// vertices = vertices.concat(path1.vertices);
-//
-// var path2 = {
-//   vertices: [
-//   vec3(3.0, -50.0, 0.001),
-// 	vec3(3.0, 50.0, 0.001),
-// 	vec3(5.0, 50.0, 0.001),
-// 	vec3(5.0, -50.0, 0.001)
-//   ],
-//   NV: 4,
-//   color: vec4(0.0, 0.0, 0.0, 1.0),
-//   startIndex: 8,
-//   draw: function(){
-//     gl.uniform4fv(colLoc, flatten(this.color));
-//     gl.drawArrays(gl.TRIANGLE_FAN, this.startIndex, this.NV);
-//   }
-// };
-// vertices = vertices.concat(path2.vertices);
-
+var grass = new Rect( vec3(0, 0, 0), grassSize, grassSize, vec4(0.4, 0.8, 0.2, 1.0) );
+//paths
+var path1 = new Rect( vec3(-5, 0, 0.001), 2, grassSize, vec4(0.0, 0.0, 0.0, 1.0) );
+var path2 = new Rect( vec3(5, 0, 0.001), 2, grassSize, vec4(0.0, 0.0, 0.0, 1.0) );
+//parthenon
+var parthenonScale = 1.4;
+var parthenon = new Parthenon(vec3(0, grassSize/2-(Parthenon.LENGTH/2*parthenonScale), Parthenon.HEIGHT/2*parthenonScale), parthenonScale);
 
 window.onload = function init() {
+    genTrees();
     clickToPlay = document.getElementById("clickToPlay");
     canvas = document.getElementById( "gl-canvas" );
     gl = WebGLUtils.setupWebGL( canvas );
@@ -123,41 +54,35 @@ window.onload = function init() {
     gl.clearColor( 0.6, 0.8, 1.0, 1.0 );
     gl.enable(gl.DEPTH_TEST);
 
-    //
-    //  Load shaders and initialize attribute buffers
-    //
+
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
     var vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
-    // gl.bufferData( gl.ARRAY_BUFFER, sizeof['vec3'] * (grass.NV + path1.NV), gl.STATIC_DRAW);
-    // gl.bufferSubData( gl.ARRAY_BUFFER, 0, flatten(grass.vertices) );
-    // gl.bufferSubData( gl.ARRAY_BUFFER, grass.NV*sizeof['vec3'], flatten(path1.vertices) );
-    //gl.bufferSubData( gl.ARRAY_BUFFER, sizeof['vec3']*NVground, flatten(Block.modelVertices) );
 
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
+    // uniforms
     colLoc = gl.getUniformLocation( program, "color" );
     worldViewLoc = gl.getUniformLocation( program, "worldView" );
     modelViewLoc = gl.getUniformLocation( program, "modelView" );
     projLoc = gl.getUniformLocation( program, "projection" );
 
+    // create and send perspective projection
     var projection = perspective(fovy, aspectRatio, near, far);
     gl.uniformMatrix4fv(projLoc, false, flatten(projection));
-
 
     window.onkeydown = function(event) {
       if(paused)
         return;
 
   		var key = String.fromCharCode(event.keyCode);
-  	  var fowardV = subtract(at, eye);
-      var fowardDirectionV = normalize(fowardV);
-      var rightDirectionV = cross(fowardV, up);
+  	  // var fowardV = subtract(at, eye);
+      // var rightDirectionV = cross(fowardV, up);
   		switch( key ) {
   		  case 'W':
           foward = true;
@@ -171,20 +96,20 @@ window.onload = function init() {
   		  case 'D':
           right = true;
   		    break;
-  		  case 'Q':
-          var newDirV = add(at, normalize(rightDirectionV) );
-          at = subtract(newDirV, at);
-          at = scale(fowardV.length, at);
-          at = newDirV;
-  		    break;
-  		  case 'E':
-          var newDirV = subtract(at, normalize(rightDirectionV) );
-          at = subtract(newDirV, at);
-          at = scale(fowardV.length, at);
-          at = newDirV;
-  		    break;
+          // dont do rotation bellow as it is done with mouse
+  		  // case 'Q':
+        //   var newDirV = add(at, normalize(rightDirectionV) );
+        //   at = subtract(newDirV, at);
+        //   at = scale(fowardV.length, at);
+        //   at = newDirV;
+  		  //   break;
+  		  // case 'E':
+        //   var newDirV = subtract(at, normalize(rightDirectionV) );
+        //   at = subtract(newDirV, at);
+        //   at = scale(fowardV.length, at);
+        //   at = newDirV;
+  		  //   break;
   		}
-  		//  render();
   	};
 
     document.onkeyup = function(event){
@@ -193,6 +118,7 @@ window.onload = function init() {
       if(event.keyCode == 27){
         paused = true;
         clickToPlay.style.display = "";
+        return;
       }
 
       switch(key){
@@ -211,6 +137,7 @@ window.onload = function init() {
       }
     };
 
+    // cross compatible pointer lock
     canvas.requestPointerLock = canvas.requestPointerLock ||
                             canvas.mozRequestPointerLock;
 
@@ -231,7 +158,7 @@ window.onload = function init() {
     var y = e.movementY;
 
     var fowardV = subtract(at, eye);
-    var fowardDirectionV = normalize(fowardV);
+    // var fowardDirectionV = normalize(fowardV);
     var rightDirectionV = cross(fowardV, up);
 
     // x movement
@@ -245,124 +172,84 @@ window.onload = function init() {
     // at = add(newDirV, at);
     // at = scale(fowardV.length, at);
     // at = newDirV;
-
-    // TODO: could have at that only gets effected by x. and atLook that gets effected by x and y. then use at to move, and atLook to do look at stuff
-
-    // render();
   };
-
-  genTrees();
 
   render();
 }
 
-var testCircle = new Circle(vec4(1,1,1,1));
-var s = mat4();
-s[0][0] = 4;
-s[1][1] = 4;
-s[2][2] = 4;
-var rs = mult( rotate(90, [1, 0, 0]) , s);
-var trs = mult( translate(0, 0, 3.7), rs);
-testCircle.trs = trs;
-
-var testCylinder = new Cylinder(vec4(1,1,1,1));
-s = mat4();
-s[0][0] = 4;
-s[1][1] = 4;
-s[2][2] = 4;
-var rs = mult( rotate(90, [1, 0, 0]) , s);
-var trs = mult( translate(0, 0, 3.7), rs);
-testCylinder.setTrs(trs);
-
-var testCone = new Cone(vec4(1,1,1,1));
-s = mat4();
-s[0][0] = 4;
-s[1][1] = 4;
-s[2][2] = 4;
-var rs = mult( rotate(90, [1, 0, 0]) , s);
-var trs = mult( translate(10, 0, 3.7), rs);
-testCone.setTrs(trs);
-
 function update(){
+  var moveLeft = left && !right;
+  var moveRight = right && !left;
+  var moveFoward = foward && !backward;
+  var moveBackward = backward && !foward;
+
+  // exit if no movement
+  if( !(moveLeft || moveRight || moveFoward || moveBackward) )
+    return;
+
   var fowardV = subtract(at, eye);
   var fowardDirectionV = normalize(fowardV);
   var rightDirectionV = cross(fowardV, up);
   var speed = 0.4;
 
-  if( (left && !right && (foward || backward) ) ||
-      (right && !left && (foward || backward) )   ){
-        speed *= 0.707;
-      }
-
+  // keep speed the same when moving diagonal
+  if( (moveLeft || moveRight) && (moveFoward || moveBackward) ){
+    speed *= 0.707;
+  }
 
   var fowardAdd = scale(speed, fowardDirectionV);
   var sideAdd = scale(speed, rightDirectionV);
 
-  if(foward && !backward){
+  if(moveFoward){
     at = add(at, fowardAdd);
     eye = add(eye, fowardAdd);
-  } else if(backward && !foward){
+  } else if(moveBackward){
     at = subtract(at, fowardAdd);
     eye = subtract(eye, fowardAdd);
   }
 
-  if(left && !right){
+  if(moveLeft){
     at = subtract(at, sideAdd);
     eye = subtract(eye, sideAdd);
-  } else if(right && !left){
+  } else if(moveRight){
     at = add(at, sideAdd);
     eye = add(eye, sideAdd);
   }
-}
-
-// loc, diam, height
-var testTree = new Tree( vec2(0, 0), 1.3, 4);
-
+} // end update()
 
 function render() {
   gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   update();
 
+  // set worldview
   worldView = lookAt(eye, at, up);
   gl.uniformMatrix4fv(worldViewLoc, false, flatten(worldView));
 
-
   // draw grass
   grass.draw();
-
   // draw paths
   path1.draw();
   path2.draw();
-
-  // testCircle.draw();
-
-  // testCylinder.draw();
-
-  // testCone.draw();
-
-  // testTree.draw();
-
+  // draw trees
   for(var i = 0; i < trees.length; ++i)
     trees[i].draw();
-  // console.log(trees.length);
-
-  // gl.uniformMatrix4fv(modelViewLoc, false, flatten(mat4()) );
-  // gl.uniform4fv(colLoc, flatten(vec4(1,1,1,1)));
-  // gl.drawArrays(gl.TRIANGLE_STRIP, testRectStartIndex, 4);
-
+  // draw parthenon
   parthenon.draw();
 
+  // only update if not paused, otherwise cannot be moving
   if(!paused)
     requestAnimFrame(render);
 }
 
 function genTrees(){
+  // try to add 1000 trees
   for(var i = 0; i < 1000; ++i){
     // diameter = x + diameter * 4
     // loc is in the center of the tree
     var fits = false;
     var tries = 0;
+    // keep trying to place this tree while it doesnt fit
     while(!fits){
       ++tries;
       if(tries > 50)
@@ -383,18 +270,18 @@ function genTrees(){
           fits = false;
 
       }
-      // check not on track
-      if(loc[0] + diam*1 > path1.loc[0] && loc[0] - diam*1 < path1.loc[0] + path1.width ||
-      loc[0] + diam*1 > path2.loc[0] && loc[0] - diam*1 < path2.loc[0] + path2.width) {
+      // check not on a track
+      if(loc[0] + diam*1 > path1.loc[0]-path1.width/2 && loc[0] - diam*1 < path1.loc[0]+path1.width/2 ||
+      loc[0] + diam*1 > path2.loc[0]-path2.width/2 && loc[0] - diam*1 < path2.loc[0]+path2.width/2) {
         fits = false;
       }
-
       //check not colliding with building
-      if(loc[0] + diam*4 > parthenon.loc[0] && loc[0] - diam*4 < parthenon.loc[0] + parthenon.width &&
-         loc[1] + diam*4 > parthenon.loc[1] && loc[1] - diam*4 < parthenon.loc[1] + parthenon.length ){
+      if(loc[0] + diam*4 > parthenon.loc[0]-parthenon.width/2 && loc[0] - diam*4 < parthenon.loc[0] + parthenon.width/2 &&
+         loc[1] + diam*4 > parthenon.loc[1]-parthenon.length/2 && loc[1] - diam*4 < parthenon.loc[1] + parthenon.length/2 ){
         fits = false;
       }
 
+      // if it fits then add it
       if(fits){
         var height = Math.random()*3+2.5;
         trees.push( new Tree(loc, diam, height) );
@@ -402,4 +289,4 @@ function genTrees(){
     } // end while
   } // end for
 
-} // end func
+} // end genTrees()
